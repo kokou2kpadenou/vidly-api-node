a3a3d215b3c4fefd4227c19c2b37b9bc3d278a42
kokou kpadenou <kkokou@gmail.com>
2020-02-10 18:58:50 -0500

    changed mongoose connection option

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,15 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
+  const db = config.get("db");
+  mongoose
+    .connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
     .then(() => winston.info(`Connected to ${db}...`));
-}
+};
