8bda955b10c0a47691a17b2aaaf65614d01b9ecb
kokou kpadenou <kokou2kpadenou@gmail.com>
2021-05-08 14:04:11 -0400

    use of mongoose transaction
    use of mongoose transction instead of fawn

--- routes/rentals.js
+++ routes/rentals.js
@@ -3,16 +3,11 @@ const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const auth = require("../middleware/auth");
 const mongoose = require("mongoose");
-const Fawn = require("fawn");
 const express = require("express");
 const router = express.Router();
 
-Fawn.init(mongoose);
-
 router.get("/", async (req, res) => {
-  const rentals = await Rental.find()
-    .select("-__v")
-    .sort("-dateOut");
+  const rentals = await Rental.find().select("-__v").sort("-dateOut");
   res.send(rentals);
 });
 
@@ -33,26 +28,26 @@ router.post("/", auth, async (req, res) => {
     customer: {
       _id: customer._id,
       name: customer.name,
-      phone: customer.phone
+      phone: customer.phone,
     },
     movie: {
       _id: movie._id,
       title: movie.title,
-      dailyRentalRate: movie.dailyRentalRate
-    }
+      dailyRentalRate: movie.dailyRentalRate,
+    },
   });
 
   try {
-    new Fawn.Task()
-      .save("rentals", rental)
-      .update(
-        "movies",
-        { _id: movie._id },
-        {
-          $inc: { numberInStock: -1 }
-        }
-      )
-      .run();
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    await rental.save();
+    await Movie.findOneAndUpdate(
+      { _id: movie._id },
+      { $inc: { numberInStock: -1 } }
+    );
+
+    session.commitTransaction();
 
     res.send(rental);
   } catch (ex) {
